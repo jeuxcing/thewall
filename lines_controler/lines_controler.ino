054f9e00be1f0e9ab8869a0b0b44792567f7f9cd
Yoann Dufresne <yoann.dufresne0@gmail.com>
2021-08-28 18:31:32 +0200

    Network pin update

--- lines_controler/lines_controler.ino
+++ lines_controler/lines_controler.ino
@@ -1,27 +1,58 @@
 #include <PJONSoftwareBitBang.h>
 #include <Adafruit_NeoPixel.h>
 
-#define REFRESH_DELAY 50
-#define NB_LINES 3
-#define NB_SEGMENTS (NB_LINES - 1)
-#define NB_LED_SEGMENT (24 /* To be changed for real usage */)
+#define REFRESH_DELAY 30
+#define NB_LINES 5
+#define NB_SEGMENTS 5
+#define NB_LED_SEGMENT (12)
 #define NB_LEDS_LINE (NB_SEGMENTS * NB_LED_SEGMENT)
+
+#define NET_ID 'R'
 
 int pins[5] = {2, 3, 5, 6, 7};
 Adafruit_NeoPixel lines[NB_LINES];
 
-#define NET 13
+#define NET 12
 PJONSoftwareBitBang bus;
 
 
-void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &info) {
-  uint8_t line_idx = payload[0];
-  uint8_t led_idx = payload[1];
-  uint8_t red = payload[2];
-  uint8_t green = payload[3];
-  uint8_t blue = payload[4];
+void modify_led(uint8_t * payload, uint8_t length) {
+  uint8_t line_idx = payload[1];
+  uint8_t led_idx = payload[2];
+  uint8_t red = payload[3];
+  uint8_t green = payload[4];
+  uint8_t blue = payload[5];
 
   lines[line_idx].setPixelColor(led_idx, red, green, blue);
+}
+
+
+void modify_segment(uint8_t * payload, uint8_t length) {
+  uint8_t line_idx = payload[1];
+  uint8_t start_led_idx = payload[2];
+  uint8_t stop_led_idx = payload[3];
+  uint8_t red = payload[4];
+  uint8_t green = payload[5];
+  uint8_t blue = payload[6];
+
+  for (uint8_t i=start_led_idx ; i<= stop_led_idx ; i++)
+    lines[line_idx].setPixelColor(i, red, green, blue);
+}
+
+
+void receiver_function(uint8_t * payload, uint16_t length, const PJON_Packet_Info &info) {
+  //Serial.println("Packet received");
+  
+  
+  uint8_t command = payload[0];
+  switch(command) {
+    case 'L':
+      modify_led(payload, length);
+      break;
+    case 'S':
+      modify_segment(payload, length);
+      break;
+  }
 };
 
 
@@ -39,10 +70,12 @@ void setup() {
   // Init network
   // Set the pin 12 as the communication pin
   bus.strategy.set_pin(NET);
-  bus.set_id(12);
+  bus.set_id(NET_ID);
   
   // Define network callback
   bus.set_receiver(receiver_function);
+
+  Serial.begin(115200);
 }
 
 unsigned long previous_show = 0;
